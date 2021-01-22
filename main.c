08e95ad5540266d9d8b052d8deb79042e1104836
walid <portawalid@gmail.com>
2021-01-22 14:39:55 +0100

    apload2

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+int inttobin(int A, int B)
+{
+
+int A[10],B,i;
+
+printf("Enter the number: ");
+scanf("%d",&n);
+
+}
 
 int convertBinToInt(char num[4]){
 

b133208a8fafc345db626fc1399b50cc79b0c174
dell-hamza <hahi18025@gmail.com>
2021-01-22 15:39:34 +0100

    walid zwin

--- main.c
+++ main.c
@@ -19,6 +19,22 @@ return 0;
 int intsomme(char num[4]){
 
 
+int a[10],n,i;
+system ("cls");
+printf("Enter the number to convert: ");
+scanf("%d",&n);
+for(i=0;n>0;i++)
+{
+a[i]=n%2;
+n=n/2;
+}
+printf("\nBinary of Given Number is=");
+for(i=i-1;i>=0;i--)
+{
+printf("%d",a[i]);
+}
+
+}
 
 
 return ;
